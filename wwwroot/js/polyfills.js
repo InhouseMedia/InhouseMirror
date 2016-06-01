dae851ae15e83965ab46faa926c7bb0478027ae2
Ramon Klanke <info@inhousemedia.nl>
2016-06-01 11:37:23 +0200

    Implement string format polyfill

---
+++ wwwroot/js/polyfills.js
@@ -0,0 +1,17 @@
+// Format text
+if (!String.prototype.format) {
+  String.prototype.format = function() {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function(match, number) { 
+      return typeof args[number] != 'undefined'
+        ? args[number]
+        : match
+      ;
+    });
+  };
+}
+
+// Console
+if(!window.console){
+	window.console = {};
+}
